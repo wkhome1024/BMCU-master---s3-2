e6581ca01aa21ff24c9266840a1cdf66ae011477
wkhome1024 <quckly1024@gmail.com>
2025-11-03 20:17:04 +0800

    更新集线器编号为2，启用抓包模式

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ char topic[8][8] = {"bmcu1-1", "bmcu1-2", "bmcu1-3", "bmcu1-4", "bmcu1-5", "bmcu
 char host_name[20] = "bmcu-hub-1"; // 设备主机名
 #define product_id "bmcu-hub"          // 产品ID
 #define device_id "s3"                 // 设备ID
-uint8_t hub_num = 1;              // 集线器编号
+uint8_t hub_num = 2;              // 集线器编号
 char mqtt_id[20];
 int save_count = 0;
 int postMsgId = 0;              // 消息ID初始值为0
 int catch_key = 0;              // 抓包计数
-bool catch_mode = false;        // 抓包模式
+bool catch_mode = true;        // 抓包模式
 bool server_key = false;        // HTTP服务器开关
 WiFiClient espclient;           // 创建一个WiFiClient对象
 PubSubClient client(espclient); // 创建一个PubSubClient对象
